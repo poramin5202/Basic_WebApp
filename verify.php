a3a2466539fd10d8e655ca6d1ba1885aac987a80
poramin5202 <poramin5202@gmail.com>
2022-09-01 13:52:15 +0700

    add post.php verify.php editlogin.html

---
+++ verify.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+<h1 style="text-align: center;"> Webboard God God  </h1> 
+    <hr> 
+    <div align="center">
+        เข้าสู้ระเบบด้วย<br>
+        Login = <?php echo $_POST["login"]; ?><br>
+        Password = <?php echo $_POST["pwd"]; ?> <br>
+    </div>
+
+
+</body>
+</html>
